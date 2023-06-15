63d19a4a50e4fa4d1b58138a00e3fb6df18cf98e
sharansalian <sharansalian@gofynd.com>
2023-06-15 16:57:41 +0530

    Initial commit

---
+++ Toaster/src/main/java/in/co/potlee/toaster/Toaster.kt
@@ -0,0 +1,11 @@
+package `in`.co.potlee.toaster
+
+import android.content.Context
+import android.widget.Toast
+
+
+object ToasterMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
